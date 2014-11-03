a87626f97d6526e50a18dd2937f59de8ec036160
sshusain <sshusain@umich.edu>
2014-11-03 03:37:35 -0500

    Fixed Parser errors, implemented db api

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var startupAPI=require('./apihelper');
 
 function collect() {
     var ret = {};
@@ -24,39 +25,61 @@ function changeToNum(obj){
 
 function getProp(req){
     var reqObj = {} ;
+	console.log(req.query);
+	var query2={};
+	for(var prop in req.query)
+	{
+		var lowprop=prop.toLowerCase();
+		if(typeof req.query[prop] === 'object')
+		{
+			if(query2[lowprop]==undefined)
+				query2[lowprop]=req.query[prop];
+			else if(req.query[prop] instanceof Array)
+			{
+					query2[lowprop]=req.query[prop].concat(query2[lowprop]);
+			}
+			else
+			{
+				for(var j in query2[lowprop])
+					req.query[prop][j]=query2[lowprop][j];
+				query2[lowprop]=req.query[prop]
+			}
+		}
+		else
+		{
+			if(query2[lowprop]!=undefined)
+				query2[lowprop]=[req.query[prop]].concat(query2[lowprop]);
+			else
+				query2[lowprop]=[req.query[prop]]
+		}
+	}
+	req.query=query2;
     for (var propName in req.query) {
-
-        var lowerPropName = propName.toLowerCase();
-
-        if (typeof(reqObj[lowerPropName]) != 'undefined') {
-            if (typeof req.query[propName] === 'object'){
-                reqObj[lowerPropName] = collect(reqObj[lowerPropName],changeToNum(req.query[propName]));
-            }
-            else{
-                reqObj[lowerPropName] = reqObj[lowerPropName].concat([req.query[propName].toLowerCase()]);
-            }
-        }
-
-
-        else {
-            if (typeof req.query[propName] === 'object') {
-                reqObj[lowerPropName] = {};
+            if (!(req.query[propName] instanceof Array)) {
+                reqObj[propName] = {};
                 for (var each in req.query[propName]) {
-                    reqObj[lowerPropName][each] = parseFloat(req.query[propName][each]);
+                    reqObj[propName][each] = parseFloat(req.query[propName][each]);
                 }
             }
             else {
-                reqObj[lowerPropName] = [req.query[propName].toLowerCase()];
+                reqObj[propName] = req.query[propName];
+				if(propName=='reqvar')
+				{
+					for(var j in reqObj[propName])
+					reqObj[propName][j]=reqObj[propName][j].toLowerCase();
+				}
             }
-        }
     }
     console.log(reqObj);
     return reqObj;
 }
 
 router.get('/request', function(req, res){
-	res.send(getProp(req));
+ 	process.on('uncaughtException', function (err) {
+	  res.send("ERROR: Invalid Request")
+	  console.log(err);
+	}); 
+	startupAPI(req.db, getProp(req), res);
 });
 
 module.exports = router;
-
