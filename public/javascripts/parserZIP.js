c2ebf6cf57ee6e7f19aa62a94d9e7f61cde37535
sshusain <sshusain@umich.edu>
2016-05-09 23:46:01 -0400

    blob to file conversion

--- public/javascripts/parserZIP.js
+++ public/javascripts/parserZIP.js
@@ -1,8 +1,26 @@
+var callBack 
 
-function preparseZIP(file, array)
+var ZIPParseHelper = function(j, entries)
 {
+	if(j==entries.length)
+		callBack();
+	else
+	{
+		entries[j].getData(new zip.BlobWriter(), function(fileIndex){return function(blob) {
+			console.log(new File([blob],entries[fileIndex].filename ))
+			ZIPParseHelper(j+1, entries)
+		}}(j))
+	}
+}
+
+function preparseZIP(file, array, fileList, callback)
+{
+	callBack = callback;
 	console.log('preParseZIP', file)
 	zip.createReader(new zip.BlobReader(file), function(zipReader) {
-					zipReader.getEntries(function(a){console.log(a)});
+					zipReader.getEntries(function(entry){
+					console.log(entry)
+						ZIPParseHelper(0, entry);
+					});
 				}, function(error){console.error(error)});
 }
