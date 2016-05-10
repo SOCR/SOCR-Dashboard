eb7d072de5cb7f4e16d5ec9fb2b56a0df7795cae
sshusain <sshusain@umich.edu>
2016-05-09 22:03:00 -0400

    basic zip parsing

--- public/javascripts/parserZIP.js
+++ public/javascripts/parserZIP.js
@@ -1,5 +1,7 @@
 function preparseZIP(file, array)
 {
 	console.log('preParseZIP')
-	
+	zip.createReader(new zip.BlobReader(file), function(zipReader) {
+					zipReader.getEntries(function(a){console.log(a)});
+				}, onerror);
 }
